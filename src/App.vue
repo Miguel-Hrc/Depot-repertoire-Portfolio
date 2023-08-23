7f0e57db48383aaeffae929aae221f4a605ff927
Miguel-Hrc <136330905+Miguel-Hrc@users.noreply.github.com>
2023-08-23 17:07:32 +0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,18 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
+  <header>
+    <div>
+      h1 class="logo" title="Logo" > Mig's Portfolio</h1>
+    </div>
+    <div>
+      <nav>
+        <a href="#">Présentation</a>
+        <a href="#">Création</a>
+        <a href="#">Contact</a>
+      </nav>
+    </div>
+  </header>
 </template>
 
 <style scoped>
