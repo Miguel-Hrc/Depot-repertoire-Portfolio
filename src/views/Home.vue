9c7ea1cb211b0f1d5db5a73c6b8118b7d07fdf86
Miguel-Hrc <136330905+Miguel-Hrc@users.noreply.github.com>
2023-08-23 20:50:35 +0400

    Add the component on the "Home"

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,15 @@
 <script setup>
+import Présentation from '../components/Présentation.vue'
 </script>
 
 <template>
+  <div id="Présentation"></div><Présentation />
 </template>
 
 <style scoped>
+
+template {
+    width: 100vw;
+}
+
 </style>
