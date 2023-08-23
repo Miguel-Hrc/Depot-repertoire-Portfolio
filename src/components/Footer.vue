e535bf6638d8ed711b9bc7250a204b7fe2a0952f
Miguel-Hrc <136330905+Miguel-Hrc@users.noreply.github.com>
2023-08-23 20:01:09 +0400

    Create the "footer" components

---
+++ src/components/Footer.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="socialNetwork">
+        <a href="https://www.linkedin.com/school/centre-europeen-de-formation/"><img src="../assets/img/logoLinkedin.png" class="logo_Linkedin" alt="image du logo Linkedin, lien" title="Lien Linkedin"></a>
+        <a href="https://github.com/Miguel-Hrc"><img src="../assets/img/logoGitHub.png" class="logo_Github" alt="image du logo GitHub, lien" title="Lien GitHub"></a>
+    </div>
+    <div>
+        <p class="Update">Dernière mise à jour le<time datetime="2023-08-19"> 19 aout 2023</time>.</p>
+    </div>
+</template>
+
+<style>
+
+
+
+.socialNetwork {
+	display: flex;
+	flex-direction: row;
+	justify-content: center;
+    text-align: center;
+    box-sizing: border-box;
+	padding-top: 1%;
+}
+
+.logo_Linkedin {
+	margin-top: 1vh;
+	width: 6vh;
+}
+
+.logo_Github {
+    margin-top: 0.5vh;
+	width: 7.2vh;
+}
+
+.Update {
+	font-family: 'Roboto Slab', serif;
+	text-align: center;
+    padding-bottom: 1vh;
+}
+
+
+
+</style>
