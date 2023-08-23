6c4fb3b242d3e135329da5ba8809d9c4e3a0d346
Miguel-Hrc <136330905+Miguel-Hrc@users.noreply.github.com>
2023-08-23 22:19:29 +0400

    Add the component on the "Home"

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Présentation from '../components/Présentation.vue'
+import Création from '../components/Création.vue'
 </script>
 
 <template>
   <div id="Présentation"></div><Présentation />
+  <div id="Création"></div><Création />
 </template>
 
 <style scoped>
@@ -13,3 +15,4 @@ template {
 }
 
 </style>
+
