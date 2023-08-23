a5751c26cbf111f706132c93fe243c6ec84fd30a
Miguel-Hrc <136330905+Miguel-Hrc@users.noreply.github.com>
2023-08-23 20:26:04 +0400

    make a home "page"

--- src/router/index.js
+++ src/router/index.js
@@ -2,9 +2,11 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-
-  ]
+  routes: [{
+      path: '/Home',
+      name: 'Home',
+      component: () => import('../views/Home.vue')
+    }]
 })
 
 export default router
