2dd9d5dc9509ea837487f0c6bed3ce782e5df467
Miguel-Hrc <136330905+Miguel-Hrc@users.noreply.github.com>
2023-08-24 12:02:06 +0400

    Make a second view to simulate an error 404

---
+++ src/views/Page_inutile.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="error">
+        <iframe src="https://giphy.com/embed/FUKCPzVj0GGrCsdsmP" width="50%" height="auto" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/colibridigital-colibrito-colibr-FUKCPzVj0GGrCsdsmP">via GIPHY</a></p>
+        <p class="error-texte">Error 404</p>
+    </div>
+</template>
+
+<style>
+
+.error {
+    display: flex;
+    flex-direction: row;
+    padding-top: 20vh;  
+    padding-left: 2vw;
+    height: 73vh;
+    color: white;
+}
+
+.error-texte {
+    font-size: 4vh;
+    position: absolute;
+    left: 70%;
+    font-family: 'Dancing Script', cursive;
+}
+
+</style>
