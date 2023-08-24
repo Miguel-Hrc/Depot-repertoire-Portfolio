60bf289167054fb44c5ca737eae71ff365b7145c
Miguel-Hrc <136330905+Miguel-Hrc@users.noreply.github.com>
2023-08-24 12:02:49 +0400

    Make a second view to simulate an error 404

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,11 @@ const router = createRouter({
       path: '/Home',
       name: 'Home',
       component: () => import('../views/Home.vue')
+    }
+    {
+      path: '/Page_inutile',
+      name: 'Page_inutile',
+      component: () => import('../views/Page_inutile.vue')
     }]
 })
 
